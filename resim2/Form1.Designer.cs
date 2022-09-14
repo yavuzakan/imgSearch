1862079fd10f519c3cf1e9aef748050796739d1e
yavuz akan <yavuz.akan@gmail.com>
2022-09-14 09:30:20 +0300

    Proje dosyası ekle.

---
+++ resim2/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿
+namespace resim2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewLinkColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewImageColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7});
+            this.dataGridView1.Location = new System.Drawing.Point(-2, 84);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(706, 451);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.DoubleClick += new System.EventHandler(this.dataGridView1_DoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(301, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(636, 23);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.ErrorImage = global::resim2.Properties.Resources.ara;
+            this.pictureBox1.Image = global::resim2.Properties.Resources.ara;
+            this.pictureBox1.Location = new System.Drawing.Point(663, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(29, 23);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.ErrorImage = global::resim2.Properties.Resources.ara;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(12, 41);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(43, 37);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox3.ErrorImage = global::resim2.Properties.Resources.ara;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(649, 41);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(43, 37);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(301, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "label2";
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "";
+            this.Column7.Name = "Column7";
+            this.Column7.Width = 21;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "";
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 21;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "";
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 21;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 21;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 21;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "";
+            this.Column2.Name = "Column2";
+            this.Column2.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column2.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.Column2.Width = 21;
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.HeaderText = "";
+            this.Column1.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Zoom;
+            this.Column1.Name = "Column1";
+            this.Column1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::resim2.Properties.Resources._2222;
+            this.ClientSize = new System.Drawing.Size(704, 534);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewImageColumn Column1;
+        private System.Windows.Forms.DataGridViewLinkColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+    }
+}
+
---
+++ resim2/Form1.cs
@@ -0,0 +1,479 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.IO;
+
+namespace resim2
+{
+    public partial class Form1 : Form
+    {
+
+        SQLiteConnection conn;
+        SQLiteCommand cmd;
+        SQLiteDataReader dr;
+        string path = "database.db";
+        string cs = @"URI=file:"+Application.StartupPath+"\\database.db";
+        int sayi2 = 0;
+       
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+            Create_db();
+            dataGridView1.RowTemplate.Height = 35;
+            
+            
+            label1.Visible = false;
+            label2.Visible = false;
+            gizle();
+
+            data_show2(0);
+            data_show2(0);
+
+
+            //Create right click menu..
+            ContextMenuStrip s = new ContextMenuStrip();
+
+            // add one right click menu item named as hello           
+            ToolStripMenuItem gizleme = new ToolStripMenuItem();
+            gizleme.Text = "Hide";
+
+            // add the clickevent of hello item
+            gizleme.Click += gizleme_Click;
+
+            // add the item in right click menu
+            s.Items.Add(gizleme);
+
+
+
+            ToolStripMenuItem goster = new ToolStripMenuItem();
+            goster.Text = "Show";
+
+            // add the clickevent of hello item
+            goster.Click += goster_Click;
+
+            // add the item in right click menu
+            s.Items.Add(goster);
+
+
+            ToolStripMenuItem txtyaz = new ToolStripMenuItem();
+            txtyaz.Text = "Import";
+
+            // add the clickevent of hello item
+            txtyaz.Click += txtyaz_Click;
+
+            // add the item in right click menu
+            s.Items.Add(txtyaz);
+
+
+
+            // attach the right click menu with form
+            this.ContextMenuStrip = s;
+
+
+
+        }
+            void goster_Click(object sender, EventArgs e)
+                {
+                    goster();
+                }
+
+            void gizleme_Click(object sender, EventArgs e)
+            {
+                gizle();
+            }
+        void txtyaz_Click(object sender, EventArgs e)
+        {
+            txtyaz();
+        }
+
+        private void calistir()
+        {
+
+          
+            string q = "";
+            try
+            {
+                String komut = '"' + label1.Text + '"';
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                process.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                process.StartInfo.FileName = "cmd.exe";
+                process.StartInfo.Arguments = "/C " + komut;
+
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardInput = true;
+                process.Start();
+
+
+
+
+                while (!process.HasExited)
+                {
+                    q += process.StandardOutput.ReadToEnd();
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+                q += "error";
+              
+            }
+    
+            
+
+        }
+
+        private void data_show()
+        {
+            try
+            {
+                var con = new SQLiteConnection(cs);
+                con.Open();
+
+                string stm = "select * FROM database LIMIT 10";
+                var cmd = new SQLiteCommand(stm, con);
+                dr = cmd.ExecuteReader();
+                int i = 0;
+                
+
+                while (dr.Read())
+                {
+                    //Image.FromFile(dr.GetValue(1).ToString())
+                    dataGridView1.Rows.Insert(0, Image.FromFile(dr.GetValue(1).ToString()), dr.GetValue(1).ToString(), dr.GetValue(2).ToString(), dr.GetValue(3).ToString(), dr.GetValue(4).ToString(), dr.GetValue(5).ToString(), dr.GetValue(6).ToString());
+                    dataGridView1.RowTemplate.Height = 400;
+
+                    Column1.Width = 400;
+                    Column2.LinkBehavior = LinkBehavior.SystemDefault;
+                }
+        
+
+                dr.Close();
+                dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+                dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+                dataGridView1.ReadOnly = true;
+
+                this.dataGridView1.AllowUserToAddRows = false;
+              
+            }
+            catch (Exception ex)
+            {
+
+            
+            }
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+        
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+
+        }
+
+        private void dataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+
+
+
+            label1.Text= dataGridView1.CurrentRow.Cells[1].Value.ToString();
+
+            calistir();
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sayi2 = 0;
+            data_show2(0);
+        }
+        private void txtyaz()
+        {
+            try
+            {
+
+                string dummyFileName = "Save Here";
+
+                SaveFileDialog sf = new SaveFileDialog();
+                // Feed the dummy name to the save dialog
+                sf.FileName = dummyFileName;
+                string txtyaz1 = "";
+
+                if (sf.ShowDialog() == DialogResult.OK)
+                {
+                    // Now here's our save folder
+                    txtyaz1 = Path.GetDirectoryName(sf.FileName);
+                    // Do whatever
+                }
+
+
+                string fileName = @"1.txt";
+              
+                File.Delete(fileName);
+                //Check if the file exists
+                if (!File.Exists(fileName))
+                {
+                    // Create the file and use streamWriter to write text to it.
+                    //If the file existence is not check, this will overwrite said file.
+                    //Use the using block so the file can close and vairable disposed correctly
+                    using (StreamWriter writer = File.CreateText(fileName))
+                    {
+                        writer.WriteLine(txtyaz1);
+
+
+
+
+                    }
+                   
+                    label2.Visible=true;
+                    label2.Text="Wait";
+                    run2();
+              
+                    MessageBox.Show("Ok.");
+                    label2.Visible=false;
+                }
+
+            }
+            catch (Exception)
+            { }
+
+            data_show2(0);
+
+        }
+        private void gizle()
+        {
+
+            dataGridView1.Columns[1].Visible = false;
+            dataGridView1.Columns[2].Visible = false;
+            dataGridView1.Columns[3].Visible = false;
+            dataGridView1.Columns[4].Visible = false;
+            dataGridView1.Columns[5].Visible = false;
+            dataGridView1.Columns[6].Visible = false;
+
+
+        }
+        private void goster()
+        {
+
+            dataGridView1.Columns[1].Visible = true;
+            dataGridView1.Columns[2].Visible = true;
+            dataGridView1.Columns[3].Visible = true;
+            dataGridView1.Columns[4].Visible = true;
+            dataGridView1.Columns[5].Visible = true;
+            dataGridView1.Columns[6].Visible = true;
+
+        }
+        private void data_show2(int sayi)
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Refresh();
+            try
+            {
+                sayi2 = sayi2 + sayi;
+                String ara ='%' + textBox1.Text + '%';
+                var con = new SQLiteConnection(cs);
+                con.Open();
+
+                if (sayi2 < 0)
+                {
+                    sayi2 = 0;
+                }
+
+                string stm = "select * FROM database WHERE r1 LIKE '"+ara +"' OR r2 LIKE '"+ara +"' OR r3 LIKE '"+ara +"' OR r4 LIKE '"+ara +"' OR r5 LIKE '"+ara +"' OR r6 LIKE '"+ara +"'   LIMIT "+ sayi2.ToString() +" , 10";
+              
+                var cmd = new SQLiteCommand(stm, con);
+                dr = cmd.ExecuteReader();
+                int i = 0;
+
+
+                while (dr.Read())
+                {
+                    //Image.FromFile(dr.GetValue(1).ToString())
+                    dataGridView1.Rows.Insert(0, Image.FromFile(dr.GetValue(1).ToString()), dr.GetValue(1).ToString(), dr.GetValue(2).ToString(), dr.GetValue(2).ToString());
+                    dataGridView1.RowTemplate.Height = 400;
+
+                    Column1.Width = 400;
+                    Column2.LinkBehavior = LinkBehavior.SystemDefault;
+                }
+
+
+                dr.Close();
+                dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+                dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+                dataGridView1.ReadOnly = true;
+
+                this.dataGridView1.AllowUserToAddRows = false;
+              
+
+                int sonsayi = dataGridView1.RowCount;
+
+                if (sonsayi < 10)
+                {
+                    pictureBox3.Enabled = false;
+                }
+                else {
+                    pictureBox3.Enabled = true;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+
+
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            sayi2 = 0;
+            data_show2(0);
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            data_show2(-10);
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            data_show2(10);
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                try
+                {
+
+                    sayi2 = 0;
+                    data_show2(0);
+                }
+                catch (Exception)
+                { }
+
+
+            }
+        }
+
+        private void run2()
+        {
+            var con = new SQLiteConnection(cs);
+            con.Close();
+
+            string q = "";
+            try
+            {
+                /*
+                String komut = "5.exe";
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                process.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                process.StartInfo.FileName = "cmd.exe";
+                process.StartInfo.Arguments = "/C " + komut;
+
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardInput = true;
+                process.Start();
+
+
+
+
+                while (!process.HasExited)
+                {
+                    q += process.StandardOutput.ReadToEnd();
+                }
+                */
+                string strCmdText;
+                strCmdText= "/C 5.exe";
+                System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+
+
+            }
+            catch (Exception ex)
+            {
+
+                
+
+            }
+    
+
+
+
+        }
+        private void Create_db()
+        {
+            if (!System.IO.File.Exists(path))
+            {
+                SQLiteConnection.CreateFile(path);
+                using (var sqlite = new SQLiteConnection(@"Data Source="+ path))
+                {
+                    try
+                    {
+
+                        sqlite.Open();
+                        string sql = "CREATE TABLE database (id INTEGER, r1 TEXT UNIQUE ,  r2 TEXT,  r3 TEXT, r4 TEXT, r5 TEXT, r6 TEXT,  PRIMARY KEY(id AUTOINCREMENT))";
+                        SQLiteCommand command = new SQLiteCommand(sql, sqlite);
+
+                        command.ExecuteNonQuery();
+
+                    }
+                    catch (Exception ex)
+                    {
+
+                        MessageBox.Show(ex.ToString());
+
+                    }
+
+
+
+                }
+
+            }
+
+
+
+        }
+
+
+
+
+
+    }
+}
